61212266bee4aca576927f387246066c88356da0
Xianyang Liu <xianyang.liu@intel.com>
2020-06-24 10:08:57 +0800

    fixes

--- python/ray/resource_spec.py
+++ python/ray/resource_spec.py
@@ -121,7 +121,7 @@ class ResourceSpec(
 
         return resources
 
-    def resolve(self, is_head):
+    def resolve(self, is_head, node_ip_address=None):
         """Returns a copy with values filled out with system defaults."""
 
         resources = (self.resources or {}).copy()
@@ -130,9 +130,12 @@ class ResourceSpec(
         assert "memory" not in resources, resources
         assert "object_store_memory" not in resources, resources
 
+        if node_ip_address is None:
+            node_ip_address = ray.services.get_node_ip_address()
+
         # Automatically create a node id resource on each node. This is
         # queryable with ray.state.node_ids() and ray.state.current_node_id().
-        resources[NODE_ID_PREFIX + ray.services.get_node_ip_address()] = 1.0
+        resources[NODE_ID_PREFIX + node_ip_address] = 1.0
 
         num_cpus = self.num_cpus
         if num_cpus is None:
